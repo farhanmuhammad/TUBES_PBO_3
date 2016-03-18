b3407234a4672bf18ccd811749a0e5924f4dc653
achmadsyafri <achmadsyafri11@gmail.com>
2016-03-18 16:21:47 +0700

    Added files via upload

---
+++ Gerbong.java
@@ -0,0 +1,40 @@
+public class Gerbong {
+	private int nomorGerbong;
+	private int maxGerbong;
+	private int bebanGerbong;
+	private int nPenumpang=0;
+	
+	public Gerbong(int nomorGerbong){
+		setNomor(nomorGerbong);
+	}
+	
+	public Gerbong(int nomorGerbong, int maxGerbong) {
+		setNomor(nomorGerbong);
+		setMaxGerbong(maxGerbong);
+		
+	}
+	
+	public int getNomor(){
+		return nomorGerbong;
+	}
+	
+	public int getMaxGerbong(){
+		return maxGerbong;
+	}
+	
+	public void setNomor(int nomorGerbong){
+		this.nomorGerbong=nomorGerbong;
+	}
+	
+	public void setMaxGerbong(int maxGerbong){
+		this.maxGerbong=maxGerbong;
+	}
+	
+	public int getBebanGerbong(){
+		return this.bebanGerbong;
+	}
+	
+	public int getNPenumpang(){
+		return this.nPenumpang;
+	}
+}
